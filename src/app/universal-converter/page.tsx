02be24645753ef6cc1660621ca35f41a58f4cc26
jlgonzalezba <jlgonzalezba@gmail.com>
2025-08-29 17:40:46 -0500

    aa

--- src/app/universal-converter/page.tsx
+++ src/app/universal-converter/page.tsx
@@ -4,7 +4,7 @@ import { Sprout } from "lucide-react";
 
 export default function UniversalConverterPage() {
   // La URL donde está corriendo tu app de Streamlit (puerto por defecto 8501)
-  const streamlitUrl = "studio-ixvf53xksdscwu3rucuyry..streamlit.app";
+  const streamlitUrl = "https://studio-ixvf53xksdscwu3rucuyry.streamlit.app";
 
   return (
     <main className="flex-grow container mx-auto px-4 py-8 flex flex-col">
